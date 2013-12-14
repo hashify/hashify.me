hashify/hashify.me

commit e25d1f8355921e4207cd402ba6e9f234d1cc3355

Simplified code slightly.

hashify.js
@@ -501,7 +501,7 @@
     var
       dataTransfer = event.dataTransfer,
       url = dataTransfer.getData('URL'),
-      file, match, reader,
+      file, reader,
       insertImage = function (uri) {
         var
           value = editor.value,
@@ -536,10 +536,10 @@
       // to avoid three levels of nesting here.
       typeof FileReader === 'function' &&
       (file = dataTransfer.files[0]) &&
-      (match = /^(image|text)\//.exec(file.type))) {
+      /^(image|text)\//.test(file.type)) {
 
       reader = new FileReader();
-      if (match[1] === 'image') {
+      if (file.type === 'image') {
         reader.onload = function (event) {
           insertImage(event.target.result);
         };

commit af72caa19393f264330ff665d34d4b2c4875e025

Fixed an error introduced in e25d1f8.

hashify.js
@@ -539,7 +539,7 @@
       /^(image|text)\//.test(file.type)) {
 
       reader = new FileReader();
-      if (file.type === 'image') {
+      if (/^image/.test(file.type)) {
         reader.onload = function (event) {
           insertImage(event.target.result);
         };